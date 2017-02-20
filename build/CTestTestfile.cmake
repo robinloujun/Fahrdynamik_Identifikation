d433bf0ac9deb981514c665428f2251e7836c78b
Jun Lou <jun.lou@student.kit.edu>
2017-02-20 20:04:00 +0100

    workspace initialize

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/robin/Fahrdynamik_Identifikation/src
+# Build directory: /home/robin/Fahrdynamik_Identifikation/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/robin/Fahrdynamik_Identifikation/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/robin/Fahrdynamik_Identifikation/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/robin/Fahrdynamik_Identifikation/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/robin/Fahrdynamik_Identifikation/src:$ROS_PACKAGE_PATH"
